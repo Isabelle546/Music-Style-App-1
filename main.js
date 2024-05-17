d8b879f9647541c8328a329e6fee9a2e93d86d14
Isabelle546 <121081555+Isabelle546@users.noreply.github.com>
2024-05-17 18:17:22 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 song1="";
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function preload(){
-    song1=loadSong("spot!.mp3");
-    song2=loadSong("crazyform.mp3");
+    song1=loadSound("spot!.mp3");
+    song2=loadSound("crazyform.mp3");
 }
 
 function setup(){
@@ -12,6 +16,26 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('Model is Initialised');
+}
+
+function gotPoses(results){
+ if(results.length>0){
+    console.log(results);
+    leftWristX= results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    console.log("leftWristX = "+leftWristX+"leftWristY = "+leftWristY);
+
+    rightWristX=results[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+    console.log("rightWristX = "+rightWristX+"rightWristY = "+rightWristY);
+ }
 }
 
 function draw(){
